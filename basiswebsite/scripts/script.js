ba995d06238e99745530f4fbfb66917211ded4ff
izairaboots <izairaboots@gmail.com>
2024-11-28 17:57:32 +0100

    update

--- basiswebsite/scripts/script.js
+++ basiswebsite/scripts/script.js
@@ -1,2 +1,11 @@
 // JavaScript Document
 console.log("hi");
+
+const menuButton = document.querySelector("header > button");
+const deNav = document.querySelector("nav");
+
+function toggleMenu(){ deNav.classList.add("toonMenu");
+}
+
+menuButton.onclick = toggleMenu;
+
